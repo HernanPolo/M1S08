b11c55b1d562e7f0a04dcd8901b42ba761e63714
Hernan Polo <hernan.polo1985@gmail.com>
2023-07-19 14:42:03 -0300

    criar JSON exercicio 1

---
+++ dados.js
@@ -0,0 +1,23 @@
+const usuarios=`
+[ 
+    {
+    nome:"Hernan",
+    email:"hernan@hotmail.com",
+    senha:3456
+    }
+],
+[ 
+    {
+    nome:"Sergio",
+    email:"sergio@hotmail.com",
+    senha:5896
+    }
+],
+[ 
+    {
+    nome:"Pablo",
+    email:"pablo@hotmail.com",
+    senha:2258
+    }
+],
+`
